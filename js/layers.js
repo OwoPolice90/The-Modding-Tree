e6ce165c1da57b0d75deee7a57aeb76284362436
OwoPolice90 <74927819+OwoPolice90@users.noreply.github.com>
2022-05-06 09:40:12 -0400

    Layers

--- js/layers.js
+++ js/layers.js
@@ -25,46 +25,4 @@ addLayer("p", {
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
-    upgrades: {
-	    	11: {
-			title:"Double Points"
-			description: "Doubles your point gain"
-			cost: new Decimal(10)
-		},
-		12: {
-			title:"Prestige Power!"
-			description: "Multiplies your point gain by 1 plus 0.25 times prestige points"
-			cost: new Decimal(30)
-			unlocked(){
-				return hasUpgrade("p", 11)
-			},
-			effect() {
-				return (player[this.layer].points.add(1).pow(0.25)).add(1)
-			},
-			effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }
-		},
-		13: {
-			title:"Quintuple Points"
-			description: "Quintuples your point gain"
-			cost: new Decimal(250)
-			unlocked(){
-				return hasUpgrade("p", 12)
-			},
-		},
-		14: {
-			title:"Exponential Points"
-			description: "Exponentiates Points by 1.25"
-			cost: new Decimal(2500)
-			unlocked() {
-				return hasUpgrade("p", 13)
-			},
-		},
-		15: {
-			title:"Secondary Prestige Power!"
-			description: "Multiplies yor point gain by 1 plus prestige points"
-			cost: new Decimal(1000000)
-			unlocked() {
-				return hasUpgrade("p", 14)
-			},
-		},
-    })    		
+  
